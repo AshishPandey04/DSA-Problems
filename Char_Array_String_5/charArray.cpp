61e1e5d5b1d1653e0f6a6e4365516f00c97549b0
AshishPandey04 <0203ashish2004@gmail.com>
2024-08-04 20:00:46 +0530

    charArray

---
+++ Char_Array_String_5/charArray.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //creation
+    char arr[100];
+
+cout<<"enter :";
+cin.getline(arr,12,'\t');
+cout<<arr;
+
+
+
+
+
+
+    // cout<<"Enter your name:"<<endl;
+    // // cin>>arr;
+
+    // cin.getline(arr,100);
+
+    // cout<<"your name is :"<<arr<<endl;
+    // cout<<arr[0]<<"->"<<(int)arr[0]<<endl;
+    // cout<<arr[1]<<"->"<<(int)arr[1]<<endl;
+    // cout<<arr[2]<<"->"<<(int)arr[2]<<endl;
+    // cout<<arr[3]<<"->"<<(int)arr[3]<<endl;
+    // cout<<arr[4]<<"->"<<(int)arr[4]<<endl;
+    // cout<<arr[5]<<"->"<<(int)arr[5]<<endl;
+    
+
+
+
+
+
+
+
+    return 0;
+}
