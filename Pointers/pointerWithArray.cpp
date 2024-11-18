15035163054502477bed777e49e682e6a60ba016
AshishPandey04 <0203ashish2004@gmail.com>
2024-11-18 15:53:48 +0530

    pointers

---
+++ Pointers/pointerWithArray.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[]={100,200,300,400,500};
+    int*p=arr;
+    int* q=arr+1;
+    cout<<*q<<endl;
+    cout<<*(q+4) <<endl;
+
+
+
+
+
+
+
+
+
+    // int arr[]={10,20,30,40};
+    // int* ptr=arr;
+    // cout<<*(arr+1);
+
+    return 0;
+}
---
+++ Pointers/three.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int* p=&a;
+    int* q=p;
+
+    cout<<"a:"<<a<<endl;
+cout<<"&a:"<<&a<<endl;
+// cout<<"*a:"<<*a<<endl;
+cout<<"p:"<<p<<endl;
+cout<<"&p:"<<&p<<endl;
+cout<<"*p:"<<*p<<endl;
+cout<<"q:"<<q<<endl;
+cout<<"&q:"<<&q<<endl;
+cout<<"*q:"<<*q<<endl;
+
+    return 0;
+}
